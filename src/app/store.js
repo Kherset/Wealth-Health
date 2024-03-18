56cee5df66417d688d39cd412366dea4f6803e3c
jeremy twardowski <kherset@protonmail.com>
2024-03-18 14:49:47 +0100

    Configuration store with employee reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import employeeReducer from "../features/employeeSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    employee: employeeReducer,
+  },
 });
