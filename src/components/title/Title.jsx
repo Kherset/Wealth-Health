3481c851137a6d6b023f8336a9c7f95b67e4bf75
jeremy twardowski <kherset@protonmail.com>
2024-03-29 09:52:31 +0100

    End of the project

--- src/components/title/Title.jsx
+++
@@ -1,8 +0,0 @@
-export default function Title() {
-  return (
-    <div className="hrnet-title-and-subtitle">
-      <h1 className="hrnet-title">HRnet</h1>
-      <p className="hrnet-subtitle">Human Ressources Department</p>
-    </div>
-  );
-}
